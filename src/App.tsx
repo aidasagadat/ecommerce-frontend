c095281b859ed1d9eb5216d23cd099a45836eb2d
aidasagadat <aidasagadat355@gmail.com>
2025-03-10 23:30:26 +0500

    Initial commit - set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+import Diversity2Icon from "@mui/icons-material/Diversity2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-20">
+      <h1 className="font-bold text-5xl">Code with Aida</h1>
+
+      <Button variant="contained">Code with Aida</Button>
+
+      <Diversity2Icon></Diversity2Icon>
     </div>
   );
 }
